585455ae7d471e90c8fc6bc2f7e5b35d6231dc33
김익현/연구원/Audio Unit(ikhyeon.kim@lge.com) <ikhyeon.kim@lge.com>
2023-02-22 20:34:50 +0900

    Add nemo.cpp, nemo.hpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//#include "nemo.hpp"
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ nemo.hpp
@@ -0,0 +1,15 @@
+//nemonemo header
+
+#ifndef NEMO_H
+#define NEMO_H
+
+
+class Nemologic{
+private:
+    int size;
+
+public:
+    Nemologic();
+};
+
+#endif
